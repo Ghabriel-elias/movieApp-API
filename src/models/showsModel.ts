e4db1d0650c282a2d2d275d21684d710082ea5a9
ghabriel <ghsouza1013@gmail.com>
2024-07-22 22:47:49 -0400

    feat: change in show model for have avarage

--- src/models/showsModel.ts
+++ src/models/showsModel.ts
@@ -1,8 +1,13 @@
 import { v4 as uuidv4 } from 'uuid';
 
 const shows = [
-    {id: uuidv4(), name: 'Godzila', releaseDate: '07-21-2021', ratingNote: 4.5, ratingComments: ['Achei o filme muito bom'], posterUrl: 'qualquerLink'}
+    {id: uuidv4(), name: 'Godzila', releaseDate: '07-21-2021', ratingStar: 4.5, rating: [{comments: 'Achei o filme muito bom',ratingNode: 4.8,userId: 12}, {comments: 'Achei o filme muito bosta',ratingNode: 1.2,userId: 122}], posterUrl: 'qualquerLink'},
+    {id: uuidv4(), name: 'Vingadores', releaseDate: '07-21-2021', ratingStar: 4.5, rating: [{comments: 'Achei o filme muito bom',ratingNode: 5,userId: 12}, {comments: 'Achei o filme muito bosta',ratingNode: 1,userId: 122}], posterUrl: 'qualquerLink'}
 ]
+
+function calculateAverage(ratingNotes: number[]) {
+    return ratingNotes.reduce((accum, value) => accum += value, 0).toFixed(1)
+}
 
 function getAllShows() {
     return shows
