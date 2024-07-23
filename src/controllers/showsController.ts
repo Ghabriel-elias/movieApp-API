991eb1ff88bf64d834d565731072efffdb244d74
ghabriel <ghsouza1013@gmail.com>
2024-07-22 22:48:20 -0400

    feat: filter and get shows

--- src/controllers/showsController.ts
+++ src/controllers/showsController.ts
@@ -1,10 +1,20 @@
-import { getAllShows } from "../models/showsModel";
+import { HttpError } from "../erros/HttpError";
+import { getAllShows, searchShow } from "../models/showsModel";
 import { Request, Response } from 'express';
 
+// GET /api/shows
 function getShows(req: Request, res: Response) {
     const shows = getAllShows()
     res.json(shows)
 }
 
-export {getShows}
+// GET /api/shows/search=:search?
+function filterShows(req: Request, res: Response) {
+    const searchTerm = req.params.search
+    if(!searchTerm) throw new HttpError(400, 'Invalid search term')
+    const showsFilter = searchShow(searchTerm)
+    res.json(showsFilter)
+}
+
+export {getShows, filterShows}
 
