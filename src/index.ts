f9c9d109ee8b39685cc76b50a05e04a4bfab0015
ghabriel <ghsouza1013@gmail.com>
2024-07-22 22:50:19 -0400

    feat: add use middlewares routes and error and dotenv config

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
+import 'dotenv/config'
 import express from 'express'
-import { api } from './routes/api.routes'
+import { apiRouter } from './routes/api.routes'
+import { errorMiddleware } from './middlewares/errorMiddleware'
+import { authRouter } from './routes/auth.routes'
 
 const app = express()
 
 app.use(express.json())
-app.use('/api', api)
+app.use('/auth', authRouter)
+app.use('/api', apiRouter)
+app.use(errorMiddleware)
 
 const PORT = process.env.PORT || 3031
 
@@ -25,3 +30,6 @@ app.listen(PORT, () => {
 // Usuário convidado por buscar/ver detalhes do filme
 
 // Usuário logado pode avaliar com estrela e comentário e favoritar o filmes
+
+// mudar o objeto de filmes pra ter um array de ratings/aviações de objetos, onde o objeto vai ter a estrela e o comentário de um usuário
+// e cada estrela adicionada no array, vou recalcular a média de estrelas e sempre retornar essa média
