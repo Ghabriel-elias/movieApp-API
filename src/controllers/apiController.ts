c76b1184d4401107a6aed3aa444a85cc2cf96bd6
ghabriel <ghsouza1013@gmail.com>
2024-07-28 16:15:44 -0400

    feat: add new controllers for search and filter shows

--- src/controllers/apiController.ts
+++ src/controllers/apiController.ts
@@ -1,20 +1,47 @@
 import { HttpError } from "../erros/HttpError";
-import { getAllShows, searchShow } from "../models/showsModel";
+import { getAllGenres } from "../models/genreModel";
+import { getAllMovies, getMoviesByGenre, searchMovie } from "../models/moviesModel";
 import { Request, Response } from 'express';
+import { getAllSeries, getSeriesByGenre, searchSeries } from "../models/seriesModel";
 
-// GET /api/shows
-function getShows(req: Request, res: Response) {
-    const shows = getAllShows()
-    res.json(shows)
+// GET /api/genres
+function getGenres(req: Request, res: Response) {
+    const genres = getAllGenres()
+    res.json(genres)
+}
+
+// GET /api/movies/:genre?
+function getMovies(req: Request, res: Response) {
+    const {genre} = req.params as {genre?: string}
+    if(genre && genre != 'all') {
+        res.json(getMoviesByGenre(genre))
+    } else {
+        res.json(getAllMovies())
+    }
+}
+
+// GET /api/movies/:genre?
+function getSeries(req: Request, res: Response) {
+    const {genre} = req.params as {genre?: string}
+    if(genre && genre != 'all') {
+        res.json(getSeriesByGenre(genre))
+    } else {
+        res.json(getAllSeries())
+    }
 }
 
 // GET /api/shows/search=:search?
 function filterShows(req: Request, res: Response) {
     const searchTerm = req.params.search
     if(!searchTerm) throw new HttpError(400, 'Invalid search term')
-    const showsFilter = searchShow(searchTerm)
-    res.json(showsFilter)
+    const moviesFilter = searchMovie(searchTerm)
+    const seriesFilter = searchSeries(searchTerm)
+    const response = {
+        series: seriesFilter,
+        movies: moviesFilter
+    }
+    res.json(response)
 }
 
-export {getShows, filterShows}
+export {getMovies, filterShows, getGenres, getSeries}
 
